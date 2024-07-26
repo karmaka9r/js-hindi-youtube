ab50caf7b81b3bbb200f5d519aaac97930a9c6fe
karmaka9r <145636958+karmaka9r@users.noreply.github.com>
2024-07-26 02:30:14 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all JS CODE AS newer version
+// alert(3+3)  we are usingnodejs not browser
+
+
+// number => 2 to the power 53
+// Bigint
+// string =>""
+// Boolean => true/false
+// null => standalone Value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // object
+console.log(typeof null) //undefined
