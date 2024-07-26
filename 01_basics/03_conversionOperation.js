5e68057787a4a93efef57dc7326c6743e95e984c
karmaka9r <145636958+karmaka9r@users.noreply.github.com>
2024-07-26 09:41:25 +0530

    Update 03_conversionOperation.js

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -17,3 +17,26 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+//******************OPERATIONS*********************
+
+console.log(2+2)
+console.log(2*2)
+console.log(2**2)
+console.log(2-2)
+console.log(2/3)
+console.log(2%3)
+
+
+let str1 = "hello"
+let str2 = "hitesh"
+let str3 = (str1 + str2)
+console.log(str3)
+
+console.log("1"+2)
+console.log(1 + "2")
+console.log("1"+2+3)
+console.log(1 + 2 + "3")
+
+console.log(+true)
+
