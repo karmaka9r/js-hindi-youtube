73373504b76f35e26cbeffd45c550122888c5c39
karmaka9r <145636958+karmaka9r@users.noreply.github.com>
2024-07-26 02:31:16 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144536
+let accountEmail = "karmak@gmail.com"
+var accountPassword = "123456"
+accountCity = "jaipur"
+let accountdate
+
+accountEmail = "akash@gmail.com"
+accountPassword = "abcd"
+accountCity = "bengaluru"
+
+
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+*/
+console.log(accountId)
+console.table([accountId, accountEmail , accountPassword, accountCity, accountdate])
